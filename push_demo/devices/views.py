27b76fbc3c11934844481aa665a492311cd060c4
brthtimea <brth.timea@gmail.com>
2017-05-17 16:20:45 +0300

    [PUSH_NOTIFICATION]: Addressed code review comments

--- push_demo/devices/views.py
+++ push_demo/devices/views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.csrf import csrf_exempt
-from push_notifications.models import GCMDevice
+from push_notifications.models import GCMDevice, APNSDevice
 from push_demo.videos.models import Video
 from django.core import serializers
 from django.shortcuts import render
@@ -10,16 +10,25 @@ from django.dispatch import receiver
 
 
 # csrf_exempt decorator is used to disable CSRF protection for this view
+DEVICE_TYPE = {'APNS': 'ios', 'FCM': 'android'}
 @csrf_exempt
 def index(request):
     if request.method == 'POST':
         token = request.POST['token']
+        device = request.POST['device']
+        print(request.POST)
         # list of all the registered devices
-        registered_devices = [device["registration_id"] for device in GCMDevice.objects.values('registration_id')]
-        print(type(registered_devices))
-        if token not in registered_devices:
-            # create new entry in the DB
-            GCMDevice.objects.create(registration_id=token, cloud_message_type="FCM")
+        if device == DEVICE_TYPE['APNS']:
+            registered_devices_apns = APNSDevice.objects.values_list('registration_id', flat=True)
+            if token not in registered_devices_apns:
+                # create new entry in the DB
+                APNSDevice.objects.create(registration_id=token, device_id=device)
+        elif device == DEVICE_TYPE['FCM']:
+            registered_devices_fcm = GCMDevice.objects.values_list('registration_id', flat=True)
+            if token not in registered_devices_fcm:
+                # create new entry in the DB
+                GCMDevice.objects.create(registration_id=token, device_id=device, cloud_message_type="FCM")
+
     context = {'data_context': 'This text is rendered from the devices template'}
     return render(request, 'devices/index.html', context)
 
@@ -28,10 +37,11 @@ def index(request):
 # in the case of a new entry or update  all the registered devices are getting notified
 @receiver(post_save, sender=Video)
 def observe_video(sender, **kwargs):
-    registered_devices = [device['registration_id'] for device in GCMDevice.objects.values('registration_id')]
     latest_video = [Video.objects.latest('created_at')]
     video_content = serializers.serialize('json', latest_video)
-    for device_id in registered_devices:
-        fcm_device = GCMDevice.objects.get(registration_id=device_id)
-        fcm_device.send_message(video_content)
+    fcm_devices = GCMDevice.objects.values_list('registration_id', flat=True)
+    #apns_devices =  APNSDevice.objects.values_list('registration_id', flat=True)
+    # Sends a notification to one or more registration_ids
+    fcm_devices.send_message(video_content)
+    #apns_devices.send_message(video_content)
     print ('Notification was sent to the registered devices')
